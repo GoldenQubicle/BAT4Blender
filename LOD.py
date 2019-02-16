499c5980bdda6e4a2a74558bd696c78bbbbc32ce
vortext <GoldenQubicle@gmail.com>
2019-02-16 17:06:24 +0100

    clean up & TODOs

--- LOD.py
+++ LOD.py
@@ -1,25 +1,23 @@
-from typing import List, Any
-from bpy_extras.io_utils import unpack_list, unpack_face_list
-
 import bpy
-
 from mathutils import Vector
-from mathutils import Matrix
+from typing import List, Any
 
 
 def get_all_bound_boxes() -> List:
-    bboxes: List[List[Any]] = []
+    # TODO figure out if splines curves etc have bound boxes
+    b_boxes: List[List[Any]] = []
     for ob in bpy.context.collection.all_objects:
         if ob.type == 'MESH':
             bbox_corners = [ob.matrix_world @ Vector(corner) for corner in ob.bound_box]
-            bboxes.append(bbox_corners)
-    return bboxes
+            b_boxes.append(bbox_corners)
+    return b_boxes
 
 
-def get_min_max_xyz(bboxes: List[List[Any]]) -> List[Any]:
+def get_min_max_xyz(b_boxes: List[List[Any]]) -> List[Any]:
+    # TODO handle error when model is offset from world origin
     (min_x, max_x, min_y, max_y, min_z, max_z) = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-    for bb in bboxes:
-        for v in bb:
+    for b in b_boxes:
+        for v in b:
             if v[0] < min_x:
                 min_x = v[0]
             if v[0] > max_x:
@@ -38,23 +36,6 @@ def get_min_max_xyz(bboxes: List[List[Any]]) -> List[Any]:
 bb = get_all_bound_boxes()
 min_max_xyz = get_min_max_xyz(bb)
 print(min_max_xyz)
-
-# (min_x, max_x, min_y, max_y, min_z, max_z) = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-# for b in bb:
-#     print("bound box")
-#     for v in b:
-#         if v[0] < min_x:
-#             min_x = v[0]
-#         if v[0] > max_x:
-#             max_x = v[0]
-#         if v[1] < min_y:
-#             min_y = v[1]
-#         if v[1] > max_y:
-#             max_y = v[1]
-#         if v[2] < min_z:
-#             min_z = v[2]
-#         if v[2] > max_z:
-#             max_z = v[2]
 
 
 # start with min / max x,y,z variables at 0 (i.e. world origin)
