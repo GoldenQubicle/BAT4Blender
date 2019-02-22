24c1c707cfc07c3d3f1117395bf8db42031edcb8
vortext <GoldenQubicle@gmail.com>
2019-02-22 20:56:22 +0100

    poc render with lod in top left corner

--- Renderer.py
+++ Renderer.py
@@ -26,39 +26,39 @@ for v in coordinates:
     for f in v:
         co_list.append(f)
 
-r = cam.camera_fit_coords(depsgraph, co_list)
+os_lod = cam.camera_fit_coords(depsgraph, co_list)[1]
 
 # calculate the orthographic scale according to gmax
 targetWidth2 = lod.dimensions[0] + 8
 renderFov = 2 * (atan(targetWidth2 / 190.0))
-os = (cam.location[2] + lod.dimensions[2] / 2) * tan(renderFov);
-cam.data.ortho_scale = os
+os_gmax = (cam.location[2] + lod.dimensions[2] / 2) * tan(renderFov) # assuming the gmax camera focus in on lod center height which seems correct
 
 print("actual ortho scale")
-print(r[1])
+print(os_lod)
 print("gmax ortho scale")
-print(os)
+print(os_gmax)
 
 # if the orthographic scale for the LOD is bigger than gmax derived value (which only uses LOD width)
 # we know the object does not fit into camera view, either depth or height wise
 # not sure yet if the derived os should increased, or if some kind of 'true' os should be used. .
-# in addition the render dimensions should be increased in this step* as well
-while r[1] > os:
-    os *= 2
+# in addition the render dimensions should be adjusted accordingly in this step* as well
+while os_lod > os_gmax:
+    os_gmax *= 2
 
 # set the os for the camera, and reset the offsets
-cam.data.ortho_scale = os
+cam.data.ortho_scale = os_gmax
 cam.data.shift_x = 0.0
 cam.data.shift_y = 0.0
 
-# get the 2d camera coordinates for the LOD
+# get the 2d camera view coordinates for the LOD
 coords_2d = [bpy_extras.object_utils.world_to_camera_view(bpy.context.scene, cam, coord) for coord in coordinates]
 
 # given its boxy nature we know the outer left and top vertex in the camera view
-# grab these two, which are in 0..1 range so convert to render dimensions output (see step* above)
+# grab these two, which are in 0..1 range, and convert to render dimensions output (see step* above)
+# now we know in pixels how far the LOD is from the left and top edges
 x_left = coords_2d[0][0] * 256
 y_top = coords_2d[2][1] * 256
-# now we know in pixels how far the render is from the left and top edges
+
 slop = 2  # keep a 2 pixel distance from edge, can be made variable later for different levels
 # finally map the pixel values back to a 0..1 range to use as offset
 x_d = translate(x_left - slop, 0, 256, 0.0, 1.0)
